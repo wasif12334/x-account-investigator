399e08e908e5466c6a47b47498e3b89dc62ed6ff
wasif12334 <kashifaliraza95@gmail>
2026-09-11 13:37:35 +0500

    adde the ui and readme file to the project after final completetion

--- x-account-investigator/Agents/BaseAgent.py
+++ x-account-investigator/Agents/BaseAgent.py
@@ -1,19 +1,24 @@
+import os
 from dotenv import load_dotenv
-load_dotenv()
 
 from langchain_tavily import TavilySearch
 from langchain_google_genai import ChatGoogleGenerativeAI
+
+load_dotenv()
 
 
 class BaseAgent:
 
     def __init__(self):
+
         self.search_tool = TavilySearch(
             max_results=5
         )
 
         self.llm = ChatGoogleGenerativeAI(
-            model="gemini-2.5-flash"
+            model="gemini-2.5-flash",
+            google_api_key=os.getenv("GOOGLE_API_KEY"),
+            temperature=0
         )
 
     def search(self, query: str):

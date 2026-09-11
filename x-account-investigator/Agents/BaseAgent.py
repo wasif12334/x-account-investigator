50da7d41f10a40c5185259212b47edb88d9cf65f
wasif12334 <kashifaliraza95@gmail>
2026-09-11 15:24:35 +0500

    reslove the issue of api - model configuration after deployemnt

--- x-account-investigator/Agents/BaseAgent.py
+++ x-account-investigator/Agents/BaseAgent.py
@@ -1,24 +1,19 @@
-import os
 from dotenv import load_dotenv
+load_dotenv()
 
 from langchain_tavily import TavilySearch
 from langchain_google_genai import ChatGoogleGenerativeAI
-
-load_dotenv()
 
 
 class BaseAgent:
 
     def __init__(self):
-
         self.search_tool = TavilySearch(
             max_results=5
         )
 
         self.llm = ChatGoogleGenerativeAI(
-            model="gemini-2.5-flash",
-            google_api_key=os.getenv("GOOGLE_API_KEY"),
-            temperature=0
+            model="gemini-3.7-flash"
         )
 
     def search(self, query: str):
